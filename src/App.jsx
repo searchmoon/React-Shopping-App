abf3e6d38bb110ae0d448bcd9a9bfe2d81ab88f0
Rumi <moonding901212@gmail.com>
2023-08-23 19:28:08 +0900

    eslint, prettier 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Layout from './components/layout/Layout';
+import CartPage from './pages/CartPage';
+import DetailPage from './pages/DetailPage';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import NotFoundPage from './pages/NotFoundPage';
+import OrderPage from './pages/OrderPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+        <Route index element={<HomePage />} />
+        <Route path="product/:id" element={<DetailPage />} />
+        <Route path="cart" element={<CartPage />} />
+        <Route path="Login" element={<LoginPage />} />
+        <Route path="register" element={<RegisterPage />} />
+        <Route path="order" element={<OrderPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/cart-empty/CartEmpty.jsx
@@ -0,0 +1,8 @@
+
+const CartEmpty = () => {
+  return (
+    <div>CartEmpty</div>
+  )
+}
+
+export default CartEmpty
